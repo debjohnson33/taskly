4cec8eaa925353f40cae573397bdabe36b8af646
Deb Johnson <1.deborah.r.johnson@gmail.com>
2025-06-06 22:12:23 -0500

    Add shopping list item

---
+++ components/ShoppingListItem.tsx
@@ -0,0 +1,78 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Alert, TouchableOpacity } from "react-native";
+import { theme } from "../theme";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+type Props = {
+  name: string;
+  isCompleted?: boolean;
+};
+
+export function ShoppingListItem({ name, isCompleted }: Props) {
+  const handleDelete = () => {
+    Alert.alert(
+      `Are you sure you want to delete ${name}`,
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        // eslint-disable-next-line prettier/prettier
+      ]
+    );
+  };
+  return (
+    <>
+      <View
+        style={[
+          styles.itemContainer,
+          isCompleted ? styles.completedContainer : undefined,
+        ]}
+      >
+        <Text
+          style={[
+            styles.itemText,
+            isCompleted ? styles.completedText : undefined,
+          ]}
+        >
+          {name}
+        </Text>
+        <TouchableOpacity activeOpacity={0.8} onPress={handleDelete}>
+          <AntDesign
+            name="closecircle"
+            size={24}
+            color={isCompleted ? theme.colorGrey : theme.colorRed}
+          />
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="auto" />
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 18,
+    paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
+  itemText: { fontSize: 18, fontWeight: 200 },
+  completedText: {
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorGrey,
+    color: theme.colorGrey,
+  },
+});
