e1c1c38137235694e4c6c41c571ec081b5f8b233
Deb Johnson <1.deborah.r.johnson@gmail.com>
2025-06-06 22:11:45 -0500

    Change root component folder

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { View, StyleSheet } from "react-native";
+
+import { ShoppingListItem } from "../components/ShoppingListItem";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ShoppingListItem name="Coffee" />
+      <ShoppingListItem name="Tea" isCompleted />
+      <ShoppingListItem name="Sugar" isCompleted />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+});
