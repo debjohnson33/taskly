9e12aed8097d2c8f7914edf9d4dc9c5878a6829f
Deb Johnson <1.deborah.r.johnson@gmail.com>
2025-07-09 15:47:35 -0500

    Add history screen

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,10 +1,43 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { countdownStorageKey, PersistedCountdownState } from ".";
+import { useState, useEffect } from "react";
+import { getFromStorage } from "../../utils/storage";
+import { format } from "date-fns";
+import { theme } from "../../theme";
+
+const fullDateFormat = "LLL d yyyy, h:mm aaa";
 
 export default function HistoryScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
+    };
+    init();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Idea</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      data={countdownState?.completedAtTimestamps}
+      ListEmptyComponent={() => (
+        <View style={styles.listEmptyContainer}>
+          <Text>No history</Text>
+        </View>
+      )}
+      renderItem={({ item }) => {
+        return (
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>
+              {format(item, fullDateFormat)}
+            </Text>
+          </View>
+        );
+      }}
+    />
   );
 }
 
@@ -14,7 +47,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontSize: 24,
+  contentContainer: {
+    marginTop: 8,
+  },
+  listItem: {
+    backgroundColor: theme.colorLightGrey,
+    marginHorizontal: 8,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 8,
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  listEmptyContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 18,
   },
 });
